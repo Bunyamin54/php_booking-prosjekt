43822de2d2c66ed32a164c3b240ae6f1d50cb993
aslnthir <aslnthir@gmail.com>
2024-10-01 13:46:47 +0200

    added om page

--- om.php
+++ om.php
@@ -10,10 +10,9 @@
   <link rel="stylesheet" href="css/styles.css">
   <?php require_once './/inc/links.php'; ?>
   <style>
-    .pop:hover{
+    .box{
        border-top-color: var(--teal) !important;
-       transform: scale(1.03);
-       transition: all 0.3s;
+       
     }
   </style>
 
@@ -26,19 +25,33 @@
    <?php require_once './/inc/header.php'; ?>
 
 
+  <div class="my-5 px-4">
   
+  <h2 class="fw-bold h-font text-center ">Om Oss</h2>
+
+  <div class="h-line bg-white "> </div>
+
+  <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ad aliquid illum minusearum placeat veniam at sequi
+     consequuntur numquam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, id.</p>
+
+</div>
+<div class="container">
+  <div class="row justify-content-between align-items-center ">
+    <div class="col-lg-6 col-md-5 mb-4">
+  <h3 class="mb-3">Lorem ipsum dolor sit.</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint molestias eos porro possimus voluptas totam nam officiis tenetur, 
+    cumque ipsa.</p>
+    </div>
+    <div class="col-lg-5 col-md-5 mb-4">
+      <img src="images/about/about.jpg" class="w-100">
+    </div>
+  </div>
+</div>
 
 
 
+<?php require_once './/inc/footer.php'; ?>
 
 
-  
-
-
-
-
- 
-
-   <?php require_once './/inc/footer.php'; ?>
 </body>
 </html>
