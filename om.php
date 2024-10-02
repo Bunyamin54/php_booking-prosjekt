21f76318f2892dfaaca349a46a8ef81a81be5248
aslnthir <aslnthir@gmail.com>
2024-10-02 14:11:08 +0200

    om page

--- om.php
+++ om.php
@@ -37,21 +37,102 @@
 </div>
 <div class="container">
   <div class="row justify-content-between align-items-center ">
-    <div class="col-lg-6 col-md-5 mb-4">
+    <div class="col-lg-6 col-md-5 mb-4 order-lg-1 order-md-1 order-2">
   <h3 class="mb-3">Lorem ipsum dolor sit.</h3>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint molestias eos porro possimus voluptas totam nam officiis tenetur, 
     cumque ipsa.</p>
     </div>
-    <div class="col-lg-5 col-md-5 mb-4">
+    <div class="col-lg-5 col-md-5 mb-4 order-lg-2 order-md-2 order-1">
       <img src="images/about/about.jpg" class="w-100">
     </div>
   </div>
 </div>
 
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-lg-3 col-md-6 mb-4 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+      <img src="images/about/hotel.svg" width="70px">
+      <hi class="mt-3">100+ ROOMS</hi>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-4 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+      <img src="images/about/customers.svg" width="70px">
+      <hi class="mt-3">200+ CUSTOMERS</hi>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-4 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+      <img src="images/about/rating.svg" width="70px">
+      <hi class="mt-3">100+ REVIEWS</hi>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-4 px-4">
+      <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+      <img src="images/about/hotel.svg" width="70px">
+      <hi class="mt-3">100+ ROOMS</hi>
+      </div>
+    </div>
+  </div>
+</div>
+<h3 class="my-5 fw-bold h-font text-center">MANAGER TEAM</h3>
+  <div class="container px-4">
+  <div class="swiper mySwiper">
+    <div class="swiper-wrapper mb-5">
+      <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+        <img src="images/about/staff.svg" class="w-100">
+        <h5 class="mt-2">Random Name</h5>
+      </div>
+      <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+        <img src="images/about/staff.svg" class="w-100">
+        <h5 class="mt-2">Random Name</h5>
+      </div>
+      <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+        <img src="images/about/staff.svg" class="w-100">
+        <h5 class="mt-2">Random Name</h5>
+      </div>
+      <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+        <img src="images/about/staff.svg" class="w-100">
+        <h5 class="mt-2">Random Name</h5>
+      </div>
+      
+      
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
 
-
+  </div>
 <?php require_once './/inc/footer.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
+  <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      spaceBetween:40,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+
+320: {
+  slidesPerView: 1,
+
+},
+
+640: {
+  slidesPerView: 1,
+},
+768: {
+  slidesPerView: 3,
+},
+1024: {
+  slidesPerView: 4,
+},
+},
+    });
+  </script>
 
 </body>
 </html>
