53e38444409388f3b3dfa9775b3ef7a13bbeb911
Bunyamin Genc <Bunyamin.Genc@Sikri.no>
2024-10-10 20:11:48 +0200

    Legg til admin-panel og admin-dashbord. Opprettet innloggings- og utloggingssider for admin. Kobling til database opprettet og database struktur laget.

---
+++ admin/dashboard.php
@@ -0,0 +1,39 @@
+<?php 
+
+  require('inc/essentials.php');
+
+   adminLogin();
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Admin Panel - Dashboard </title>
+
+    <?php require('inc/links.php'); ?>
+</head>
+<body class="bg-light">
+    
+   <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between">
+
+
+  <h3 class="mb-0">Admin Panel</h3>
+
+   <a href="loggut.php" class="btn btn-light btn-sm">Logg ut</a>
+
+   </div>
+
+
+
+ <?php require('inc/script.php'); ?>
+</body>
+</html>
