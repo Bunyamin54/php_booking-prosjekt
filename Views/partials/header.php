9024c0069e1432336558616b4e5eced7a341bd14
Bunyamin Genc <Bunyamin.Genc@Sikri.no>
2024-10-11 13:36:22 +0200

    working on structure

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -2,26 +2,26 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
-        <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="../../public/index.php">UIA MOTEL</a>
+        <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="/php_booking-prosjekt/public/index.php">UIA MOTEL</a>
         <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active me-2" aria-current="page" href="../public/index.php">Hjem</a>
+                    <a class="nav-link active me-2" aria-current="page" href="/php_booking-prosjekt/public/index.php">Hjem</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="./users/rom.php">Rom</a>
+                    <a class="nav-link me-2" href="/php_booking-prosjekt/Views/users/rom.php">Rom</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="../users/fasiliteter.php">Fasiliteter</a>
+                    <a class="nav-link me-2" href="/php_booking-prosjekt/Views/users/fasiliteter.php">Fasiliteter</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="../users/kontakt.php">Kontakt oss</a>
+                    <a class="nav-link me-2" href="/php_booking-prosjekt/Views/users/kontakt.php">Kontakt oss</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="../users/om.php">Om</a>
+                    <a class="nav-link me-2" href="/php_booking-prosjekt/Views/users/om.php">Om</a>
                 </li>
             </ul>
             <div class="d-flex">
