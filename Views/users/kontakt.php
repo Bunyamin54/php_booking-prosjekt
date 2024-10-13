805290084df0371ec57611967bff396526e4e2c2
Bunyamin Genc <Bunyamin.Genc@Sikri.no>
2024-10-13 14:52:34 +0200

    unødvendig kommnettared slettet

--- Views/users/kontakt.php
+++ Views/users/kontakt.php
@@ -34,7 +34,7 @@
 
 
   <?php require_once '../partials/header.php'; ?>
-  <!-- <?php require_once './login_registering.php'; ?> -->
+  <!-- <?php require_once './login_registering.php'; ?>
 
   <!-- <?php require_once './fasiliteter.php'; ?> -->
 
